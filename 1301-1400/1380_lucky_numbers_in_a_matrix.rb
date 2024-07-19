dd4fd23e4ac02ad82b552ff6930590beb596093a
theforestvn88 <theforestvn88@gmail.com>
2024-07-19 10:32:28 +0700

    1380

--- 1301-1400/1380_lucky_numbers_in_a_matrix.rb
+++ 1301-1400/1380_lucky_numbers_in_a_matrix.rb
@@ -13,15 +13,23 @@ def lucky_numbers (matrix)
         end
     end
 
-    lucky_nums = []
     min_rows.each_with_index do |col_index, row_index|
-        lucky_nums << matrix[row_index][col_index] if max_cols[col_index] == row_index
+        # we can prove that there's only one lucky number 
+        # given the input matrix contains only distinct numbers
+        # now let suppose [i,j] is the lucky number
+        # we assume that there's another lucky number [u,v] 
+        # so we have: m[u][v] is the min of the row u => m[u][v] < m[u][j] < m[i][j]
+        # we also have: m[u][v] is the max of the col v => m[u][v] > m[i][v] > m[i][j]
+        # => m[u][v] < m[i][j] && m[u][v] > m[i][j] ==> wrong
+        # so either there's no [u,v] or [u,v] is [i,j], 
+        # but the matrix contains only distinct numbers, so there's only one luck number.
+        return [matrix[row_index][col_index]] if max_cols[col_index] == row_index
     end
 
-    lucky_nums
+    []
 end
 
-# better
+# more ruby style
 def lucky_numbers (input)
     input.transpose.map { |col| col.max } & input.map { |row| row.min }
 end
