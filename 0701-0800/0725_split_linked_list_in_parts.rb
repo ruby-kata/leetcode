b016bc73945cfa81e9abc59eb1143ca9a8647eaa
theforestvn88 <theforestvn88@gmail.com>
2024-09-08 09:10:15 +0700

    improve 0725

--- 0701-0800/0725_split_linked_list_in_parts.rb
+++ 0701-0800/0725_split_linked_list_in_parts.rb
@@ -10,33 +10,32 @@
 # @param {Integer} k
 # @return {ListNode[]}
 def split_list_to_parts(head, k)
-    len = 0
+    len = 0 
     p = head
     while p
         len += 1
         p = p.next
     end
 
-    parts = Array.new(k)
-    parts_len = Array.new(k, 0)
-    while len > 0
-        (0...k).each do |i|
-            break if len <= 0
-            parts_len[i] += 1
-            len -= 1
-        end
-    end
-
+    part_size = len/k
+    remain = len % k
+    parts = []
     p = head
-    parts_len.each_with_index do |l, i|
-        break if p.nil? || l <= 0
-        parts[i] = p
+    while p
+        parts << p
+        # to reach the next part ListNode, jump part_size-1 times, 
+        # since "parts occurring earlier should always have a size greater than or equal to parts occurring later"
+        # so if remain still there, jump one more time
+        (part_size + ((remain -= 1) >= 0 ? 0 : -1)).times {
+            break if p.nil?
+            p = p.next
+        }
+        break if p.nil?
 
-        (l-1).times { |_| p = p.next }
         next_p = p.next
         p.next = nil
         p = next_p
     end
 
-    parts
+    parts + [[]] * (k - parts.size)
 end
